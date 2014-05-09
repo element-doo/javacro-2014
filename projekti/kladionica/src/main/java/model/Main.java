3ac1f7bcb89fc516f9d791f36257a813711ed7ac
Davor Stimac <davor.stimac@gmail.com>
2014-05-09 19:01:50 +0200

    * example java read/write for kladionica
    Signed-off-by: Davor Stimac <davor.stimac@gmail.com>

--- projekti/kladionica/src/main/java/model/Main.java
+++ projekti/kladionica/src/main/java/model/Main.java
@@ -1,20 +1,61 @@
 package model;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.Kladionica.Listic;
+import model.Kladionica.Ponuda;
+import model.Kladionica.Tekma;
+import model.Kladionica.repositories.ListicRepository;
+import model.Kladionica.repositories.TekmaRepository;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 
 import com.dslplatform.client.Bootstrap;
 import com.dslplatform.patterns.ServiceLocator;
 
 public class Main {
-    public static void main(final String[] args) throws IOException {
+    public static void main(final String[] args) throws Exception {
         // An instance of Service Locator.
         // You can use it to fetch instances of repositories.
         final ServiceLocator locator = init();
 
 
-        // TODO: Your code here
+        Logger logger = locator.resolve(Logger.class);
+      Tekma derbi = new Tekma().setDatumTekme(DateTime.now()).setDomacin("Hajduk").setGost("Rijeka");
+      derbi.persist();
+
+      Tekma tekma = new TekmaRepository(locator).find("1001").get();
+      Ponuda ponuda1 = new Ponuda().setTekma(tekma).setTip("Iks").setKoeficijent(new BigDecimal("1.1")).setIstekla(false);
+      Ponuda ponuda2 = new Ponuda().setTekma(tekma).setTip("Jedan").setKoeficijent(new BigDecimal("1.01")).setIstekla(false);
+      Ponuda ponuda3 = new Ponuda().setTekma(tekma).setTip("Jedan").setKoeficijent(new BigDecimal("1.2")).setIstekla(false);
+
+      List<Ponuda> parovi = new ArrayList<Ponuda>();
+      parovi.add(ponuda1);
+      parovi.add(ponuda2);
+      parovi.add(ponuda3);
+
+      Listic listic = new Listic().setDatumUplate(LocalDate.now()).setIznos(new BigDecimal(100)).setParovi(parovi);
+
+      listic.persist();
+
+
+      ListicRepository lRep = new ListicRepository(locator);
+      List<Listic> listici = lRep.findAll().get();
+
+      Listic list = listici.get(0);
+
+      logger.info("LIST: \n  Datum: {}\n  Dobio?: {}\n  Iznos: {}\n  Dobivam: {}\n  UkKoef: {}"
+          , list.getDatumUplate()
+          , list.getDobitniLisic()
+          , list.getIznos()
+          , list.getMoguciDobitak()
+          , list.getUkupniKoeficijent());
+
 
 
         // Perform cleanup.
